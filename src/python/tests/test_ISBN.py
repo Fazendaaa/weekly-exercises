c750eb2e77a06eaba8088ec75533ded413c97836
Faendaaa <lucas.carotta@outlook.com>
2025-05-18 18:35:06 -0300

    Improving code standard comparison

--- src/python/tests/test_ISBN.py
+++ src/python/tests/test_ISBN.py
@@ -1,9 +1,9 @@
 from weekly_exercises.ISBN import isISBN
 
 
-def test_example_truthfully():
-    assert True == isISBN("3-598-21508-8")
+def test_example_truthfully() -> None:
+    assert isISBN("3-598-21508-8")
 
 
-def test_example_wrongfully():
-    assert False == isISBN("3-598-21508-0")
+def test_example_wrongfully() -> None:
+    assert not isISBN("3-598-21508-0")
